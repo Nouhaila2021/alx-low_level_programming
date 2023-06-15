cf189866461a8b78087fdd577c9ed06bd036312d
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-15 16:18:34 -0700

    commit about adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : print message  with puts function
+ * Return :Alawyss 0 (Sucesses)
+*/
+
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
 }
