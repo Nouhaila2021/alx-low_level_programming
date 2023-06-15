b7e42d7b0077e766d9d3c2480d2c82466ffa1975
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-15 16:44:10 -0700

    commit about adding 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description : print message  with printf function
  *
- * Return : Alawys 0 (Sucesses)
+ * Return : Alawys 0 (Success)
 */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art.\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
