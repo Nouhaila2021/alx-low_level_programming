5495921a03db79f6daeec6070494fad2744f98e6
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-17 10:53:40 -0700

    commit about adding 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<sdtlib.h>
+
+/**
+ * main - Entry Point
+ *
+ *Description: "Print alphabets in upper case amd lower case with putchar."
+ *
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c < 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
