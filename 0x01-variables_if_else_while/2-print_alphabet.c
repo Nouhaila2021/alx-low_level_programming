7edcdf276f054f4450eaaf78a2890dfcc3bfa275
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-17 09:02:13 -0700

    commit about adding 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- Entry Point
+ *
+ * Description: "Print alphabet in lowercase."
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+		putchar(' ');
+	}
+
+	return (0);
+
+}
