c5800ca07608e2463f81c496c69b5eb3861f4113
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-17 06:19:08 -0700

    commt about 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point
  *
- * Description : print sizeof char int long long int and float with printf function
+ * Description: "print sizeof char int long long int and float with printf function."
  *
- * Return : Alawys 0 (Success)
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
 {
 	printf("Size of a char: %u byte(s) \n", sizeof(char));
-	printf("Size of a int: %u byte(s) \n", sizeof(int));
+	printf("Size of an int: %u byte(s) \n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int : %u byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %u byte(s)\n", sizeof(float));
