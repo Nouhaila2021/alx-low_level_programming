70ce5a4ecda3f351993cfbd172b1d07b8451ec5a
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-21 15:37:01 -0700

    commit about adding 0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry Point
+ * Description: print _putchar
+ *
+ * Return: Always  (Success)
+*/
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
