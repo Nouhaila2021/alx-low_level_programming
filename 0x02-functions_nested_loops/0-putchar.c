fdfe9f05caa50e3164306c11ca86736a578f4e49
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 03:07:50 -0700

    commit about 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,10 @@
 
 /**
  * main - Entry Point
- * _putchar: writes the character c to stdout
- * @c: The character to print
- * Return: on success 1
- *         on error, -1 is returned, and erno is set appropriately
+ * Description: writes the character c to stdout
+ * Return: Always 0 (Succes)
+ *     
 */
-
-
 int main(void)
 {
 
