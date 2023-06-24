550f3bd243b18283ccf34fc95d7f416dac4528ab
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 04:34:40 -0700

    commit about adding 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _isalpha - check if alphabet is lowercase or uppercase
+ *                  a-z
+ * @c: input for _islower function
+ * Return: 1 if lowercase 0 is is not
+*/
+int _isalpha(int c)
+{
+
+
+	if ((c >= 97 && c <= 122) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
