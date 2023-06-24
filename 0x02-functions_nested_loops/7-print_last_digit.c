937ba29074cb8c349f6126b248d34a033fe29eaa
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 05:02:18 -0700

    commit about adding 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * print_last_digit - check if alphabet is lowercase or uppercase
+ *                  a-z
+ * @n: input for _print_sign function
+ * Return: 1 if n positive  -1 if negative and 0 if 0
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
+
