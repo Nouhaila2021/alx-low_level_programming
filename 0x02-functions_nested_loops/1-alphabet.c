3282e6f4f1c374f5cb241a92ea2d0ff32271d9f5
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 03:32:39 -0700

    commit about 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include"main.h"
-
 /**
  * print_alphabet - use _putchar to write aplhabet
  *                  a-z
 */
 void print_alphabet(void)
 {
-	int ch;
-	for (ch = 'a', ch <= 'z', ch++)
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 		_putchar('\n');
