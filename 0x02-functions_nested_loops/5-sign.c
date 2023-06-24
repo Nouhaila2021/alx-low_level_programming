45021cb1bf7ec9f73715035ecdbe64f4483c5306
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 04:47:58 -0700

    commit about adding 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _print_sign - check if alphabet is lowercase or uppercase
+ *                  a-z
+ * @n: input for _print_sign function
+ * Return: 1 if n positive  -1 if negative and 0 if 0
+*/
+int _print_sign(int n)
+{
+
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+}
