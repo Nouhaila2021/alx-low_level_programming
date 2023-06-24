8215a78a3aa872150707ab65cfa355b667e65d2f
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-24 02:44:14 -0700

    commit about c_putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,12 +2,6 @@
 #define MAIN_H
 #include<stdio.h>
 
-/**
- * _putchar - write the character c to stdout
- * 0c: The character to print
- * Return: on success 1
- *         on error, -1 is returned, and erno is set appropriately
-*/
 int _putchar(char c);
 
 int main(void);
