376aaa4a46071441f4223fec6cee31ebe714c132
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-25 04:11:29 -0700

    commit about adding 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * add - add function
+ * @m: input for add function
+ * @n: input for add  function
+ * Return: Always 0 (Success)
+*/
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
+
