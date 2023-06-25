68dc8b3e615bd5c2cd96c63cec99d4df20d65c6b
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-25 07:21:18 -0700

    commit about adding 1-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_to_98  - print number to 98
+ *
+ * @n: input for add  function
+ *
+*/
+void print_to__98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("\d", i);
+	}
+
+}
+
