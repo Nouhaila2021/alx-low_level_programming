33a70a8c4f29421d787c016377fae7f504db8dfa
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-25 03:11:46 -0700

    commit about 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * times_tables - print every minute of the day
+ * times_table - print every minute of the day
 */
 void times_table(void)
 {
@@ -8,11 +8,17 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar(48);
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((j * 9) + '0');
 			_putchar(',');
 			_putchar(' ');
+			prod = i + j;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
 		 _putchar('\n');
 	}
