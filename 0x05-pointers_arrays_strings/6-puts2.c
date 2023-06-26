598f818a52a30f2d4412cfdf06f22b1b671dc46b
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-26 06:49:15 -0700

    commit about adding 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - function print a string followed by new line
+ *
+ * @s : input for _puts
+ *
+ * Return: Nothing
+ *
+*/
+
+void _puts(char *s)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(*s + 0);
+		s++;
+	}
+	_putchar('\0');
+	_putchar('\n');
+}
