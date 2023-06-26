82a66a09329824a7ac29977225c0353abd4e31ff
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-26 04:59:15 -0700

    commit about adding 0-reste_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98:  updates the value it points to to 98
+ *
+ * @n: input for reset_to_98
+ *
+ * Return: Nothing
+*/
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
