4ce1e4eb324c0039e6e3a80ae2fe9af50bb9ceea
Nouhaila2021 <tanafaatnouhaila@gmail.com>
2023-06-26 05:13:05 -0700

    commit about adding 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swap the values of two integres
+ *
+ * @a : input for swap_int
+ * @b : input for swap_int
+ *
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = &a;
+	&a = &b;
+	&b = c;
+}
+
